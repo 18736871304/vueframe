320f7a3e6dd81f2d1d08529f5f8ef9c928686bce
18736871304 <1655299089@qq.com>
2024-11-27 14:04:38 +0800

    123312

--- vue.config.js
+++ vue.config.js
@@ -21,18 +21,18 @@ module.exports = {
     // // 热更新
     // hotOnly: false,
     // 使用代理
-    proxy: {
-      '/api': {
-        // 目标代理服务器地址
-        // target: 'https://insure.meihualife.com',
-        // target: 'http://8.133.195.79',
-        // 开启代理，本地创建一个虚拟服务器 允许跨域
-        // changeOrigin: true,
-        ws: false,
-        pathRewrite: {
-          // '^/api': '' // 重写请求
-        }
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     // 目标代理服务器地址
+    //     // target: 'https://insure.meihualife.com',
+    //     target: 'http://8.133.195.79',
+    //     // 开启代理，本地创建一个虚拟服务器 允许跨域
+    //     // changeOrigin: true,
+    //     ws: false,
+    //     pathRewrite: {
+    //       // '^/api': '' // 重写请求
+    //     }
+    //   },
+    // },
   },
 }
