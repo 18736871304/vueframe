077e66d89f1d818def8ea373a745969e67d47fcc
18736871304 <1655299089@qq.com>
2024-11-27 12:08:01 +0800

    123

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vueframe
 比较好的，比较全面的一个vue框架
+
+项目地址：
+https://18736871304.github.io/vueframe/dist/index.html
+
+![alt text](1732679247426.jpg)
--- vue.config.js
+++ vue.config.js
@@ -22,12 +22,12 @@ module.exports = {
     // hotOnly: false,
     // 使用代理
     proxy: {
-      '/': {
+      '/api': {
         // 目标代理服务器地址
         // target: 'https://insure.meihualife.com',
         target: 'http://8.133.195.79',
         // 开启代理，本地创建一个虚拟服务器 允许跨域
-        changeOrigin: true,
+        // changeOrigin: true,
         ws: false,
         pathRewrite: {
           // '^/api': '' // 重写请求
